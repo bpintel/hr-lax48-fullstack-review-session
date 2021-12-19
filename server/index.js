c5b1bad0c42bc0157771dc93f9b08ae8c3dbee3c
Ben Pintel <bpintel@gmail.com>
2021-12-19 13:04:03 -0800

    Server listening

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+// const router = require('router');
+const PORT = 3001;
+const server = express();
+
+server.use(express.json());
+server.use(express.static(path.join(__dirname, '../client/dist')));
+// server.use('/api', router);
+
+server.listen(PORT, (err) => {
+    console.log(err ? err : `Listening on port ${PORT}`);
+})

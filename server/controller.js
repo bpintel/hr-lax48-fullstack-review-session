3c66055044ba5dfdb4d26bacf7290587b517e8fd
Ben Pintel <bpintel@gmail.com>
2021-12-19 15:27:33 -0800

    fixed typo and added catch for err

--- server/controller.js
+++ server/controller.js
@@ -10,8 +10,9 @@ const controller = {
   //create
   createStudent: (req, res) => {
     console.log(req.body)
-    Student.create({ name: req.body.name, imageUrl: req.body.imageUrl})
+    Student.create({ name: req.body.name, imageUrl: req.body.url})
     .then(() => res.status(201).send(console.log(`${req.body.name} created.`)))
+    .catch(err => res.status(404).send(err))
   },
   //update
   updateStudent: (req, res) => {

01b65660b898556a400c8f4196ed21f44b211230
Ben Pintel <bpintel@gmail.com>
2021-12-19 15:46:40 -0800

    Client can delete a student. window asks for confirmation before deletion.

--- client/src/components/ListElement.jsx
+++ client/src/components/ListElement.jsx
@@ -9,10 +9,10 @@ class ListElement extends React.Component {
       editMode: false,
       input: ''
     }
-    //bind
     this.editModeToggle = this.editModeToggle.bind(this);
     this.handleChange = this.handleChange.bind(this);
     this.updateStudent = this.updateStudent.bind(this);
+    this.deleteStudent = this.deleteStudent.bind(this);
   }
 
   editModeToggle() {
@@ -34,8 +34,15 @@ class ListElement extends React.Component {
     .catch(err => console.log(err))
   }
 
-  
-
+  deleteStudent() {
+    if(confirm(`Are you sure you want to delete ${this.props.student.name}`)){
+      axios.delete(`/api/students/${this.props.student._id}`)
+      .then(() => {
+        this.props.getStudents()
+      })
+      .catch(err => console.log(err))
+    } 
+  }
 
   render() {
     return (
@@ -49,6 +56,7 @@ class ListElement extends React.Component {
         <span>
           <button onClick={this.editModeToggle}>Cancel</button>
           <button onClick={this.updateStudent}>Update</button>
+          <button onClick={this.deleteStudent}>Delete</button>
         </span>
       </div>
       
